e6d21d1e78b380ae0bdd04c19b5b3c9bf8ea4376
Kunj Patel <37160487+kunjpatel49@users.noreply.github.com>
2024-02-19 00:35:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Import your publishable key
+const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISHABLE_KEY;
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
